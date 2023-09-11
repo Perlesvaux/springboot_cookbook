5d10af0b97f741eaad1c2f3b5d46ebdb1099780e
chico <paltryhumanity@gmail.com>
2023-09-11 06:11:44 -0600

    TODO: remove duplicates

--- parser.py
+++ parser.py
@@ -1,56 +1,59 @@
 from re import findall
 from sys import argv
+
+def format_code(dump, color, template=""):
+    for item in color.matches:
+        txt=item.strip(color.markup).replace("<", "&lt;").replace(">", "&gt;")
+        dump=dump.replace(item, template.format(txt=txt, css=color.css))
+    return dump
+
+
+def add_popover(dump, text, comment, template=""):
+    for i, val in enumerate(text.matches):
+        pop=comment.matches[i].replace(comment.markup, "")
+        txt=val.strip().replace(text.markup, "")
+        dump=dump.replace(val, template.format(pop=pop, txt=txt))
+    return dump
+
+
+class color:
+    def __init__(self, doc, rgx, markup="none", css=""):
+        self.matches = findall(rgx, doc)
+        self.markup  = markup
+        self.css     = css
+
+
 if len(argv) == 2:
 
     with open(argv[1], 'r') as fp:
 
         document= fp.read()
 
-        commentary = "##.*"
-        tutorial   = ".+(?=##)"    #.+(?= \/\/)
-        link       = "\bhttps?://\S+\b"
-        javacode   = "JAVA\*\*\*.*\n"
-        jscode     = "JS\*\*\*.*\n"
+        code_tag_template    = """<code style="color:{css};">{txt}</code>"""
+        comm_tag_template    = """<span class="commented" data-toggle="popover" data-content="{pop}">{txt}</span>"""
+        link_tag_template    = """<a href="{pop}">{txt}</a>"""
+        # link_tag_template    = """<h1>{pop}{txt}</h1>"""
+        titl_tag_template    = """<h2>{txt}</h2>"""
 
-        commentaries = findall(commentary, document)
-        tutorials    = findall(tutorial, document)
-        links        = findall(link, document)
-        javacodes    = findall(javacode, document)
-        jscodes      = findall(jscode, document)
+        pink = color(document, "PNK\*\*\*.*\n", "PNK***", "pink")
+        blue = color(document, "BLU\*\*\*.*\n", "BLU***", "lightskyblue")
+        comm = color(document, "##.*"         , "##")
+        tuto = color(document, ".+(?=##)")
+        link = color(document, "###\*.+###\*"   , "###*")
+        href = color(document, "\^\^\^.+\^\^\^"       , "^^^")
+        titl = color(document, "\d+.*:")
 
-
-        if len(commentaries) != len(tutorials):
+        if len(comm.matches) != len(tuto.matches):
             print("Error: commentaries and tutorials aren't the same length!")
             exit(1)
 
-
-        #adding hyperlinks!
-        for l in links:
-            if l != "":
-                document=document.replace(l, f"""<a href="{l}">{l}</a>""")
-
-
-        #adding color to java-code!
-        for j in javacodes:
-            if j != "":
-                document=document.replace(j, f"""<code class="javacode">{j.strip("JAVA***").replace("<", "&lt;").replace(">", "&gt;")}</code>""")
-
-
-        #adding color to java-code!
-        for j in jscodes:
-            if j != "":
-                document=document.replace(j, f"""<code class="jscode">{j.strip("JS***").replace("<", "&lt;").replace(">", "&gt;")}</code>""")
-
-
-        #Now that commentaries can be retrieved from a list, let's remove them from the document's body
-        for x in commentaries:
-            if x != "":
-                document=document.replace(x, "")
-
-
-        for i, tuto in enumerate(tutorials):
-            if tuto != "" and commentaries[i] != "":
-                document=document.replace(tuto, f"""<span class="commented" data-toggle="popover"    data-content="{commentaries[i].replace('##', '')}">{tuto.strip()}</span>""")
+        document = add_popover(document, link, href, link_tag_template)
+        document = format_code(document, href)
+        document = format_code(document, pink, code_tag_template)
+        document = format_code(document, blue, code_tag_template)
+        document = format_code(document, comm)
+        document = add_popover(document, tuto, comm, comm_tag_template)
+        document = format_code(document, titl, titl_tag_template)
 
 
         bootstrap = """<meta charset="utf-8">
@@ -62,17 +65,21 @@ if len(argv) == 2:
 
         css = """<style>
 body{
-    background-color: #405d27;
+    background-color: #313636;
 }
   span{
     color:#82b74b;
+  }
+
+  h2{
+    color:olive;
   }
 
   pre{
     margin: 0 auto;
     width: fit-content;
     color:#fff;
-    background:#3e4444;
+    background:#252828;
     white-space: pre-wrap;
     border-style: none;
   }
@@ -80,16 +87,6 @@ body{
   .commented{
     width:fit-content;
     text-shadow: .1px .1px black;
-  }
-
-  .javacode{
-    color: pink;
-
-  }
-
-  .jscode{
-    color: #3399ff;
-
   }
 
   .commented:hover{
@@ -137,14 +134,17 @@ $(document).ready(function(){
            {css}
            <title>{argv[1]}</title>
         </head>
-        <body><pre><code>
+        <body>
+        <pre>
 {document}
-            </code></pre>
+        </pre>
             {JS}
         </body>
         </html>
         """
 
         print(template)
+        # print(link.matches)
+        # print(href.matches)
         # print(len(tutorials), len(commentaries))
-        # print(javacodes)
+        # print(pnks)
